61f79089df74ceedba9d1ed94f20218aa7078613
klrotex <klrotex11@gmail.com>
2024-09-03 22:28:33 +0300

    feat: add Button

--- src/shared/components/ui/button/Button.stories.tsx
+++ src/shared/components/ui/button/Button.stories.tsx
@@ -1,6 +1,5 @@
-import { Button } from './button'
+import { Button } from './Button'
 import { Meta, StoryObj } from '@storybook/react'
-import Link from 'next/link'
 import { useState } from 'react'
 
 const meta = {
---
+++ src/shared/components/ui/button/Button.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react'
+import { Slot } from '@radix-ui/react-slot'
+import { cva, type VariantProps } from 'class-variance-authority'
+import { cn } from '@/shared/utils'
+
+const buttonVariants = cva(
+  [
+    'rounded-sm',
+    'inline-flex',
+    'items-center',
+    'justify-center',
+    'px-6',
+    'py-1.5',
+    'focus:border-2',
+    'focus:border-accent-500',
+    'disabled:pointer-events-none',
+    'disabled:text-light-900',
+  ],
+  {
+    variants: {
+      variant: {
+        primary: [
+          'bg-accent-500',
+          'hover:bg-accent-100',
+          'active:bg-accent-700',
+          'disabled:bg-accent-900',
+        ],
+        secondary: ['bg-dark-300', 'hover:bg-dark-100', 'active:bg-[#212121]'],
+        outline: [
+          'border',
+          'border-accent-500',
+          'text-accent-500',
+          'hover:border-accent-100',
+          'hover:text-accent-100',
+          'active:border-accent-700',
+          'active:text-accent-700',
+          'disabled:text-accent-900',
+          'disabled:border-accent-900',
+        ],
+        link: [
+          'text-accent-500',
+          'hover:text-accent-100',
+          'active:text-accent-700',
+          'disabled:text-accent-900',
+        ],
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+    },
+  }
+)
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, asChild = false, ...props }, ref) => {
+    const Component = asChild ? Slot : 'button'
+    return (
+      <Component
+        className={cn(buttonVariants({ variant, className }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = 'Button'
+
+export { Button, buttonVariants }
