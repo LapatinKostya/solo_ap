1633d73e196a7f1efed878efbb6b740d83f32727
klrotex <klrotex11@gmail.com>
2024-09-11 23:58:47 +0300

    feat: TermsOfService page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Link href={'/termsOfService'}>Click</Link>
+      <Link href={'/privacyPolicy'}>Click</Link>
+    </div>
+  )
 }
---
+++ pages/termsOfService.tsx
@@ -0,0 +1,3 @@
+import { TermsOfService } from '@/pages/termsOfService'
+
+export default TermsOfService
